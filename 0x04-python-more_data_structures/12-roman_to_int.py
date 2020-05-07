5c3d76655a067845a1bd7dffde7c54552df82745
kefitaib <taibkefi@yahoo.fr>
2020-05-07 10:57:15 -0700

    converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     import string
-    res = 0
     for x in roman_string:
-        if x not in string.ascii_letters:
-            return res
+        if x not in string.ascii_uppercase:
+            return 0
 
+    res = 0
     num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     new = [v for x in roman_string for k, v in num.items() if x == k]
     for i in reversed(range(len(new))):
