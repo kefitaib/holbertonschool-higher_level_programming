d293b6ffc7b3fd463282da08f770e8b9409050b9
kefitaib <taibkefi@yahoo.fr>
2020-05-07 11:00:30 -0700

    returns the number of keys in a dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    res = 0
     if roman_string:
-        res = 0
         num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         new = [v for x in roman_string for k, v in num.items() if x == k]
         for i in reversed(range(len(new))):
             if i >= 1 and new[i] > new[i - 1]:
                 res -= new[i - 1] * 2
             res += new[i]
-        return res
 
-    return 0
+    return res
