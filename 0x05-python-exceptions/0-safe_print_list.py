fcbb371bede33fe3cd1aaabb9583926a42a52bb4
kefitaib <taibkefi@yahoo.fr>
2020-05-18 05:48:13 -0700

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        while my_list[i] and i < x:
+            print(my_list[i], end="")
+            i += 1
+        print()
+        return i
+
+    except IndexError:
+        print()
+        return i
