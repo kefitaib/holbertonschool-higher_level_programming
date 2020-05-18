a34f4b853a6ff16f54bdf4984eb7d04e49889d7d
kefitaib <taibkefi@yahoo.fr>
2020-05-18 08:02:14 -0700

    prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except (ValueError, TypeError) as err:
+        print("Exception: {0}".format(err))
+        return False
