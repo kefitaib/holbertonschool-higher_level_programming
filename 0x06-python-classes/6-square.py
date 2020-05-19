7c159aba4dd785bc2532c2d7c8f98c0799436856
kefitaib <taibkefi@yahoo.fr>
2020-05-19 11:00:20 -0700

    test position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,21 +9,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the data."""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-
-        else:
-            self.__size = size
-
-        if type(position) is not tuple or len(position) != 2 or\
-           type(position[0]) is not int or position[0] < 0 or\
-           type(position[1]) is not int or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """ retune the area of the sqaure
@@ -76,6 +63,6 @@ class Square:
         if type(value) is not tuple or len(value) != 2 or\
            type(value[0]) is not int or value[0] < 0 or\
            type(value[1]) is not int or value[1] < 0:
-            self.__position = value
-        else:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
