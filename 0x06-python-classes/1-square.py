1c5f8b9d031af60d1047c7f371fac01b7e276cb0
kefitaib <taibkefi@yahoo.fr>
2020-05-19 08:36:55 -0700

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" empty class Square that defines a square
+"""
+
+class Square:
+    """ class that define a Square
+    """
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
