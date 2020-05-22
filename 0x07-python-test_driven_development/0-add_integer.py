79653c18cdf044ff391f73e94ef0ccdf52a3bc55
kefitaib <taibkefi@yahoo.fr>
2020-05-22 23:41:22 +0000

    fix None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """
     adds 2 integers
     """
-    if a is None or type(a) not in [int, float]:
+    if a is None or (type(a) not in [int, float]):
         raise TypeError("a must be an integer")
 
     if type(b) not in [int, float]:
