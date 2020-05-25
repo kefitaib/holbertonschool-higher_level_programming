3cb6c6ad1cc497f123cb654016b5a5c6e9df99d8
kefitaib <taibkefi@yahoo.fr>
2020-05-25 10:47:47 -0700

    number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -18,10 +18,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """ get the width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ set the width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
 
@@ -32,23 +34,26 @@ class Rectangle:
 
     @property
     def height(self):
+        """ get the height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ set the height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
 
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
     def area(self):
+        """ return the area  """
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        """ return the perimeter """
+        if self.__width == 0 or self.height == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
