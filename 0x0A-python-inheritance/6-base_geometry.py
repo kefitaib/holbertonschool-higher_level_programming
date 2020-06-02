dbab0d15f4ecb902a356d401abc6cb93a0850a07
kefitaib <taibkefi@yahoo.fr>
2020-06-02 06:33:27 -0700

    implement a public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module - BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """ class defenition of BaseGeometry """
+
+    def area(self):
+        """ return the area """
+
+        raise Exception("area() is not implemented")
