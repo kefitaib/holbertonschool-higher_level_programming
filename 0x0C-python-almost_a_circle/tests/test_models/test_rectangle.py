7984302323dd507b9b391663a37c223a64a7a076
kefitaib <taibkefi@yahoo.fr>
2020-06-09 06:33:17 -0700

    unit test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,46 +14,62 @@ class TestBade(unittest.TestCase):
     def test_Init(self):
         """ test value  """
 
-        """  test id"""
         r1 = r(6, 2)
-        self.assertAlmostEqual(r1.id, 1)
+        self.assertAlmostEqual(r1.x, 0)
 
         r2 = r(2, 3, 3, 2)
-        self.assertAlmostEqual(r2.id, 2)
+        self.assertAlmostEqual(r2.y, 2)
 
         r3 = r(3, 6, 5, 0, 12)
         self.assertAlmostEqual(r3.id, 12)
 
+    def test_Type(self):
+        """ Errors """
+
+        with self.assertRaises(TypeError):
+            r1 = r(10, 't')
+
+        with self.assertRaises(TypeError):
+            r2 = r(10, 2, 't', 2)
+
+        with self.assertRaises(TypeError):
+            r3 = r(10, 2, 6, 2, 7, 3)
+
+        with self.assertRaises(TypeError):
+            r4 = r(10)
+
+    def test_Value(self):
+        """ Error """
+
+        with self.assertRaises(ValueError):
+            r1 = r(-12, 10)
+
+        with self.assertRaises(ValueError):
+            r1 = r(10, 2, 6, -2)
+
+    def test_Area(self):
         """ test area """
+
+        r1 = r(6, 2)
         self.assertAlmostEqual(r1.area(), 12)
+
+        r2 = r(2, 3, 3, 2)
         self.assertAlmostEqual(r2.area(), 6)
+
+        r3 = r(3, 6, 5, 0, 12)
         self.assertAlmostEqual(r3.area(), 18)
 
-        """ Errors """
-        with self.assertRaises(ValueError):
-            r4 = r(-12, 10)
-
-        with self.assertRaises(TypeError):
-            r5 = r(10, 't')
-
-        with self.assertRaises(TypeError):
-            r6 = r(10, 2, 't', 2)
-
-        with self.assertRaises(ValueError):
-            r7 = r(10, 2, 6, -2)
-
-        with self.assertRaises(TypeError):
-            r7 = r(10, 2, 6, 2, 7, 3)
-
-        with self.assertRaises(TypeError):
-            r7 = r(10)
-
+    def test_Display(self):
         """ test display """
+
+        r1 = r(6, 2)
         s = '######\n######\n'
         self.assertAlmostEqual(r1.display(), print(s))
 
+        r2 = r(2, 3, 3, 2)
         s = '\n\n   ##\n   ##\n   ##\n'
         self.assertAlmostEqual(r2.display(), print(s))
 
+        r3 = r(3, 6, 5, 0, 12)
         s = '     ###\n     ###\n     ###\n     ###\n     ###\n     ###\n'
         self.assertAlmostEqual(r3.display(), print(s))
