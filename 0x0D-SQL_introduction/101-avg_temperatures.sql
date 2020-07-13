80182964ed2594c77ada4b41cbeb5c0262fb3c6c
kefitaib <taibkefi@yahoo.fr>
2020-07-13 14:13:45 -0700

    displays the average temperature (Fahrenheit) by city ordered by temperature (descending)

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- task 18
+-- displays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending)
+SELECT city, avg(value) as avg_temp FROM temperatures GROUP BY city
+ORDER BY avg_temp DESC;
