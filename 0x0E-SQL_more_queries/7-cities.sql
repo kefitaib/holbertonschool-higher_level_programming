99cc2d8b3f3609a24f0818ed05f3199308eab6e9
kefitaib <taibkefi@yahoo.fr>
2020-07-14 05:08:01 -0700

    database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- creates the table cities
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    state_id INT, name VARCHAR(256)
+    state_id INT, name VARCHAR(256),
     FOREIGN KEY(state_id) REFERENCES states(id));
