365d3eb0168f3ac803efd7b0b3897e7e7a4fdafa
kefitaib <taibkefi@yahoo.fr>
2020-07-14 04:12:20 -0700

    creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- task 2
+-- creates the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
+
+-- create user user_0d_2
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grant privileges
+GRANT SELECT ON hbtn_0c_2.* TO user_0d_2@localhost;
+
+-- apply
+FLUSH PRIVILEGES;
