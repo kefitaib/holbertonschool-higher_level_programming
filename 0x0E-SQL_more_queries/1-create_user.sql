d551056570e5b5e4a23cd00eabbb1a38ac53d41d
kefitaib <taibkefi@yahoo.fr>
2020-07-14 04:11:12 -0700

    creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- task 1
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
 -- grant privileges
-GRANT PROXY ON root TO user_0d_1@localhost;
+GRANT ALL ON *.* TO user_0d_1@localhost;
 
 -- apply
 FLUSH PRIVILEGES;
