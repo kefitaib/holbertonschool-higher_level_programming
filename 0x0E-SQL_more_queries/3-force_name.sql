1b1e11876fae041d46065ca7b176b128b7ca1195
kefitaib <taibkefi@yahoo.fr>
2020-07-14 05:15:50 -0700

    creates the table force_name on your MySQL server.

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- task 3
 -- creates the table force_name on your MySQL server
-CREATE TABLE IF NOT EXISTS force_name (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
