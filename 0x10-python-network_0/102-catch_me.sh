63177da1566fe4aaaae06eaaf6e76e710b3a2a92
kefitaib <taibkefi@yahoo.fr>
2020-08-18 12:42:06 -0700

    last

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me
 # that causes the server to respond with a message containing You got me!
-curl -s "$1"; printf "You got me!\n"
+curl -sL "$1" -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
