ed4297c6a9dccc806893c3d29ca53c779b691376
kefitaib <taibkefi@yahoo.fr>
2020-08-18 11:28:55 -0700

    takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS google.com -i | grep Allow: | cut -d':' -f2
+curl -sX "$1" -i | grep Allow: | cut -d' ' -f2-
