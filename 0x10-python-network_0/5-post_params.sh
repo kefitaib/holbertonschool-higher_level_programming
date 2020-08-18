e9846c48adf119f17bcd68e2039873a77fbc6a21
kefitaib <taibkefi@yahoo.fr>
2020-08-18 11:40:50 -0700

    sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-curl "$1" -X POST -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD"
+curl -s "$1" -X POST -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD"
