abaa9a3c176d399b699a409e2e103b133c6620c3
kefitaib <taibkefi@yahoo.fr>
2020-08-18 11:40:06 -0700

    takes in a URL , sends a GET request and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL , sends a GET request and displays the body of the response
-curl "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
