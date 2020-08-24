c76516b994097fb4124ec17a4767f1139918ac8e
kefitaib <taibkefi@yahoo.fr>
2020-08-24 14:31:16 -0700

    that takes your Github credentials (username and password) and uses the Github API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module """
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
