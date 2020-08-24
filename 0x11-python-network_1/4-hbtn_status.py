d37d22b76da77b5e2674b6c5954a8adc3752e4ce
kefitaib <taibkefi@yahoo.fr>
2020-08-24 08:03:29 -0700

    script that fetches an url using the package requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module """
+
+
+import requests
+
+
+
+if __name__ == "__main__":
+
+    req = requests.get("https://intranet.hbtn.io/status")
+
+    print("Body response:")
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
