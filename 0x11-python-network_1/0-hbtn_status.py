3046cf49648d75eb70d1609584dedcbcb1db3a5e
kefitaib <taibkefi@yahoo.fr>
2020-08-24 06:18:09 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,9 @@ import urllib.request
 
 req = urllib.request.Request('https://intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
+    html = response.read()
+
     print("Body response:")
-    print('\t- type: {}'.format(type(response)))
-    print('\t- content: {}'.format(response.read()))
-    print('\t- utf8 content: {}'.format(response.msg))
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
