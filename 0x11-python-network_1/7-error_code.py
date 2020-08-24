f4bec64c10f743f1c030cf4f9370241bf01dbf82
kefitaib <taibkefi@yahoo.fr>
2020-08-24 08:43:22 -0700

    akes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module """
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
