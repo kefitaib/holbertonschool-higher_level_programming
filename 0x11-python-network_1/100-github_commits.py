6aa3a8be35ac144621466084e16888825bfad438
kefitaib <taibkefi@yahoo.fr>
2020-08-24 15:42:47 -0700

    Please list 10 commits (from the most recent to oldest) of the repository 'X' by the user 'X'

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     req = requests.get("https://api.github.com/repos/{}/{}/commits"
                        .format(argv[2], argv[1])).json()
 
-    i = 0
     for i, r in zip(range(10), req):
         print("{}: {}".format(r.get("sha"), r.get("commit").
                               get("author").get("name")))
