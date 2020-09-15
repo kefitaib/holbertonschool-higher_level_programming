69548fb5bf8c11fe48085451c6fd1d0e9de504f8
kefitaib <taibkefi@yahoo.fr>
2020-09-15 07:44:54 -0700

    searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,20 +4,20 @@ if (process.argv.length === 3 || process.argv.length === 2) {
 } else {
   let big;
   let big2;
-  if (process.argv[2] < process.argv[3]) {
-    big = process.argv[3];
-    big2 = process.argv[2];
+  if (parseInt(process.argv[2]) < parseInt(process.argv[3])) {
+    big = parseInt(process.argv[3]);
+    big2 = parseInt(process.argv[2]);
   } else {
-    big = process.argv[2];
-    big2 = process.argv[3];
+    big = parseInt(process.argv[2]);
+    big2 = parseInt(process.argv[3]);
   }
   for (let i = 4; i < process.argv.length; i++) {
-    if (process.argv[i] > big) {
+    if (parseInt(process.argv[i]) > big) {
       big2 = big;
-      big = process.argv[i];
+      big = parseInt(process.argv[i]);
     }
-    if (big2 < process.argv[i] && process.argv[i] < big) {
-      big2 = process.argv[i];
+    if (big2 < parseInt(process.argv[i]) && parseInt(process.argv[i]) < big) {
+      big2 = parseInt(process.argv[i]);
     }
   }
   console.log(big2);
