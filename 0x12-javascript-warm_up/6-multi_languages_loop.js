3116594fa5d4741bc61f17354d8d23e20f542cc1
kefitaib <taibkefi@yahoo.fr>
2020-09-15 06:26:02 -0700

    prints 3 lines by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const ar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-ar.forEach((s) => {
-  console.log(s);
-});
+for (let i = 0; i < 3; i++) {
+  console.log(ar[i]);
+}
