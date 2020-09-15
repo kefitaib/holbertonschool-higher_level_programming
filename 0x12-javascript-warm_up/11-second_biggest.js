3425fdbf7949cc340369a3cc0e6f08227ad90392
kefitaib <taibkefi@yahoo.fr>
2020-09-15 07:40:44 -0700

    searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+if (process.argv.length === 3 || process.argv.length === 2) {
+  console.log(1);
+} else {
+  let big;
+  let big2;
+  if (process.argv[2] < process.argv[3]) {
+    big = process.argv[3];
+    big2 = process.argv[2];
+  } else {
+    big = process.argv[2];
+    big2 = process.argv[3];
+  }
+  for (let i = 4; i < process.argv.length; i++) {
+    if (process.argv[i] > big) {
+      big2 = big;
+      big = process.argv[i];
+    }
+    if (big2 < process.argv[i] && process.argv[i] < big) {
+      big2 = process.argv[i];
+    }
+  }
+  console.log(big2);
+}
