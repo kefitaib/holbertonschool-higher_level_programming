56d21872c21031f80b53caeb3f66a0ccfd9819da
kefitaib <taibkefi@yahoo.fr>
2020-09-15 06:08:03 -0700

    script that prints My number: <first argument converted in integer>

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,10 @@
 if (process.argv[2] === undefined) {
   console.log('No number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2], 10));
+  const n = parseInt(process.argv[2], 10);
+  if (isNaN(n)) {
+    console.log('No number');
+  } else {
+    console.log('My number: ' + n);
+  }
 }
