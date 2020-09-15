10241e6bc692f68b74b0f10a4759872804525b82
kefitaib <taibkefi@yahoo.fr>
2020-09-15 07:59:32 -0700

    prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 if (process.argv[2] === undefined) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     let s = '';
-    for (let j = 0; j < process.argv[2]; j++) {
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
       s += 'X';
     }
     console.log(s);
