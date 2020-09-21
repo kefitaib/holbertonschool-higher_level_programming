95755fc663cdb1bdbecad758223b629b247489ba
kefitaib <taibkefi@yahoo.fr>
2020-09-21 11:23:01 -0700

    Print square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,14 +7,15 @@ module.exports = class Square extends Rectangle {
 
   print (c) {
     if (typeof c === 'undefined') {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let s = '';
+        for (let j = 0; j < this.width; j++) {
+          s += c;
+        }
+        console.log(s);
       }
-      console.log(s);
     }
   }
 };
