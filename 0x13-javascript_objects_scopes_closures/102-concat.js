675a9c9a84c0a0116113369e7a1177d58728e45c
kefitaib <taibkefi@yahoo.fr>
2020-09-21 15:44:32 -0700

    concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 var fs = require('fs');
-
-fs.readFile(String(process.argv[2]), function (err1, data1) {
-  fs.readFile(String(process.argv[3]), function (err2, data2) {
+fs.readFile(process.argv[2], function (err1, data1) {
+  fs.readFile(process.argv[3], function (err2, data2) {
     if (err1 || err2) {
       throw new Error();
     }
     const data = data1 + data2;
-    fs.writeFile(String(process.argv[4]), data, function (err3) {
+    fs.writeFile(process.argv[4], data, function (err3) {
       if (err3) {
         throw new Error();
       }
