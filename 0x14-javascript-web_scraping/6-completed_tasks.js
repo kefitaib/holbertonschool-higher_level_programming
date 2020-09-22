f64a54db28124b6e1d290b701f8a3444a24ef90a
kefitaib <taibkefi@yahoo.fr>
2020-09-22 11:49:27 -0700

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const res = {};
-let x = 1;
-let c = 0;
 request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   }
   const b = JSON.parse(body);
   for (const i of b) {
-    if (i.userId === x) {
-      if (i.completed === true) {
-        c++;
+    if (i.completed === true) {
+      if (res[i.userId]) {
+        res[i.userId] += 1;
+      } else {
+        res[i.userId] = 1;
       }
-    } else {
-      res[String(x)] = c;
-      c = 0;
-      if (i.completed === true) {
-        c++;
-      }
-      x = i.userId;
     }
   }
-  res[String(x)] = c;
   console.log(res);
 });
